10b219ec9491335a18c38bdeb543f6f0405deca7
MichaelDuo <michaeldong21@gmail.com>
2019-01-07 18:18:35 +0800

    Update Changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,27 +3,25 @@ import React from 'react';
 export default function Home(){
     return (
         <div>
-            <h1>MichaelDuo's Blog</h1>
-            <ul uk-accordion="true">
-                <li className="uk-open">
-                    <a className="uk-accordion-title" href="#">Item 1</a>
-                    <div className="uk-accordion-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </li>
-                <li>
-                    <a className="uk-accordion-title" href="#">Item 2</a>
-                    <div className="uk-accordion-content">
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
-                    </div>
-                </li>
-                <li>
-                    <a className="uk-accordion-title" href="#">Item 3</a>
-                    <div className="uk-accordion-content">
-                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                    </div>
-                </li>
-            </ul>
+            <div className="uk-container uk-margin-top uk-margin-bottom">
+                <h1>MichaelDuo</h1>
+                <ul uk-tab="swiping: true;" className="uk-flex-right">
+                    <li>
+                        <a href="">Blogs</a>
+                    </li>
+                    <li className="uk-active">
+                        <a href="">Archives</a>
+                    </li>
+                    <li>
+                        <a href="/c">About me</a>
+                    </li>
+                </ul>
+                <ul className="uk-switcher">
+                    <li>Blogs</li>
+                    <li>Archives</li>
+                    <li>About Me</li>
+                </ul>
+            </div>
         </div>
     )
 }
