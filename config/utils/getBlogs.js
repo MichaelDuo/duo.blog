be3ee3bfe351833034bd96ce23ef379e165d03ac
MichaelDuo <michaeldong21@gmail.com>
2019-01-08 15:31:51 +0800

    Update Config

--- config/utils/getBlogs.js
+++ config/utils/getBlogs.js
@@ -4,15 +4,20 @@ const paths = require('../paths')
 
 const blogsPath = path.resolve(paths.appPublic, 'blogs')
 
+const MARK_DOWN = /(.*)\.md$/
+
 module.exports = function getBlogs(){
     let blogFiles = []
     if(fs.existsSync(blogsPath)){
-        blogFiles = fs.readdirSync(path.resolve(paths.appPublic, 'blogs'))
+        blogFiles = fs.readdirSync(path.resolve(paths.appPublic, 'blogs')).filter(filename=>{
+            return MARK_DOWN.test(filename)
+        })
     } else {
         console.warn('Blogs directory is not found.')
     }
     const blogs = blogFiles.map(filename => {
         return {
+            id: filename.slice(0, -3),
             title: filename,
             url: path.resolve(paths.servedPath, 'blogs', filename)
         }
