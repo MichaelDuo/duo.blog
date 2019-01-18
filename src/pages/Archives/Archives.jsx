675eb4528f8422dd6475bf57a50d87f28c466e87
MichaelDuo <michaeldong21@gmail.com>
2019-01-18 16:58:41 +0800

    Updates

--- src/pages/Archives/Archives.jsx
+++ src/pages/Archives/Archives.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 
+const data = [
+    {
+        year: 2019,
+        blogs: [
+            { title: 'Blog' },
+            { title: 'Blog' },
+            { title: 'Blog' }
+        ]
+    },
+    {
+        year: 2018,
+        blogs: [
+            { title: 'Blog' },
+            { title: 'Blog' },
+            { title: 'Blog' }
+        ]
+    },
+    {
+        year: 2017,
+        blogs: [
+            { title: 'Blog' },
+            { title: 'Blog' },
+            { title: 'Blog' }
+        ]
+    }
+]
+
+function YearBlogs({year, blogs}){
+    return (
+        <div className="uk-margin">
+            <div className="uk-text-lead">
+                {year}
+            </div>
+            <ul class="uk-list uk-list-bullet">
+                {blogs.map(blog=>{
+                    return (
+                        <li key={blog.title}>
+                            <a className="uk-link-heading">{blog.title}</a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
 export default function Archives(){
     return (
         <div>
-            Archives
+            {data.map(year=>YearBlogs(year))}
         </div>
     )
 }
--- src/pages/Archives/index.js
+++ src/pages/Archives/index.js
@@ -1,1 +1,10 @@
-export {default} from './Archives'
+import {connect} from 'react-redux'
+import Archives from './Archives'
+
+const mapStateToProps = (state) => {
+  return {
+    blogsByYear: []
+  }
+}
+
+export default connect(mapStateToProps)(Archives)
